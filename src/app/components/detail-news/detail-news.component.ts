cf5691b3b53a21cdd7febf82756006a7a9658aa0
corneliusdief <cornelius@diefenthal-ww.de>
2019-07-30 15:43:07 +0200

    News implemented

--- src/app/components/detail-news/detail-news.component.ts
+++ src/app/components/detail-news/detail-news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-detail-news',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailNewsComponent implements OnInit {
 
-  constructor() { }
+  public newsID;
+  public newsData;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+
+    this.newsID = this.route.snapshot.paramMap.get('id');
+
+    this.http.get('https://hochschulsport-koeln.de/json/news/' + this.newsID + '/?_format=json')
+      .subscribe((data) => {
+        this.newsData = data;
+      });
   }
 
 }

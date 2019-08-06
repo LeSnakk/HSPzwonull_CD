6e8f63b47ad1ad6d976c97c87b4789ff62d36b8f
corneliusdief <cornelius@diefenthal-ww.de>
2019-08-06 14:26:04 +0200

    Categories implemented, detail-news fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import {Routes, RouterModule} from '@angular/router';
 import {OverviewKategoriesComponent} from "./components/overview-kategories/overview-kategories.component";
 import {HomeComponent} from "./components/home/home.component";
 import {DetailKursComponent} from "./components/detail-kurs/detail-kurs.component";
+import {OverviewKurseComponent} from './components/overview-kurse/overview-kurse.component';
+import {DetailNewsComponent} from './components/detail-news/detail-news.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'overview-categories', component: OverviewKategoriesComponent},
   {path: 'detail-kurs/:id', component: DetailKursComponent},
+  {path: 'detail-news/:id', component: DetailNewsComponent},
+  {path: 'overview-kurse/:id', component: OverviewKurseComponent},
+
 ];
 
 @NgModule({
